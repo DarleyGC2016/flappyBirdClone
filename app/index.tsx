f224a9014004e3a665432d11b37431e4e4531ef9
DarleyGC2016 <garciacardoso.darley@gmail.com>
2024-06-24 11:13:11 -0300

    chapter10 - começo e fim

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import 'react-native-reanimated';
 
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { GameEngine } from 'react-native-game-engine';
 import entities from "./entities"
@@ -10,15 +10,19 @@ import Physics from './physics'
 export default function App() { 
   const [running, setRunning] = useState<boolean>(false);
   const [gameEngine, setGameEngine] = useState<any>(null);
-  const [start, setStart] = useState(initialState)
+  const [currentPoints, setCurrentPoints] = useState<number>(0)
 
   useEffect(()=>{
-    setRunning(true);
+    setRunning(false);
   }, []);
 
   return (
-    <View>
-      <StatusBar style='auto' hidden={true}/>
+    <View>      
+      <Text style={{textAlign: 'center',
+        fontSize: 40,
+        fontWeight: 'bold',
+        margin: 20
+      }}>{currentPoints}</Text>
       <GameEngine
         ref={(ref) => { setGameEngine(ref) }}
         systems={[Physics]}
@@ -27,16 +31,40 @@ export default function App() {
         onEvent={(e: any) => {
           switch (e.type) {
             case 'game_over':
-               setRunning(false);
-               gameEngine.stop();
-              break;
-          
-            default:
+                setRunning(false);
+                gameEngine.stop();
+                break;
+            case 'new_point':
+              setCurrentPoints(currentPoints + 1);
               break;
           }
         }}
         style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+        <StatusBar style='auto' hidden={true}/>
       </GameEngine>
+        
+      {!running?
+        <View style={{
+          justifyContent: 'center',
+          alignItems: 'center'}}>
+          <TouchableOpacity 
+            style={{
+              backgroundColor: 'green',
+              paddingHorizontal: 30,
+              paddingVertical: 30
+            }}
+            onPress={() => {
+                setCurrentPoints(0);
+                setRunning(true);
+                gameEngine.swap(entities())
+            }}
+          >
+            <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
+              START GAME
+            </Text>
+          </TouchableOpacity>
+        </View> 
+        : null }
     </View>
   );
 }
--- app/physics.js
+++ app/physics.js
@@ -19,16 +19,23 @@ const Physics = (entities, {touches, time, dispatch}) => {
    Matter.Engine.update(engine, time.del1a);
 
    for (let index = 1; index < 2; index++) { 
+      if (entities[`ObstacleTop${index}`].body.bounds.max.x <= 50 && !entities[`ObstacleTop${index}`].point) {
+         entities[`ObstacleTop${index}`].point = true;
+         dispatch({type: 'new_point'})
+      }
+
+
       if (entities[`ObstacleTop${index}`].body.bounds.max.x <= 0) {
             const pipeSizePos = getPipeSizePosPair(windowWidth * 0.9);
             Matter.Body.setPosition(entities[`ObstacleTop${index}`].body, pipeSizePos.pipeTop.pos);
-            Matter.Body.setPosition(entities[`ObstacleBottom${1}`].body, pipeSizePos.pipeBottom.pos);
+            Matter.Body.setPosition(entities[`ObstacleBottom${index}`].body, pipeSizePos.pipeBottom.pos);
+            entities[`ObstacleTop${index}`].point = false;
       }     
       Matter.Body.translate(entities[`ObstacleTop${index}`].body, {x: -3,y: 0});
-      Matter.Body.translate(entities[`ObstacleBottom${1}`].body, {x: -3,y: 0});
+      Matter.Body.translate(entities[`ObstacleBottom${index}`].body, {x: -3,y: 0});
    }
 
-   Matter.Events.on(engine, 'collisionsStart', (event) => {
+   Matter.Events.on(engine, 'collisionStart', (event) => {
       dispatch({ type : 'game_over'});
    })
 
